07ceca7bd6e9ab5c75cb38c24658b42f034fc94c
tanghaifeng <tanghaifeng-gz@loongson.cn>
2012-07-23 10:42:25 +0800

    修改ls1b nand驱动，使用动态分配内存.

--- sys/dev/nand/ls1g-nand.c
+++ sys/dev/nand/ls1g-nand.c
@@ -850,25 +850,28 @@ static void ls1g_nand_init_info(struct ls1g_nand_info *info)
 
 int ls1g_nand_pmon_info_init(struct ls1g_nand_info *info, struct mtd_info *mtd)
 {
-//	info->drcmr_dat =  ((unsigned int)(malloc(sizeof(struct ls1g_nand_dma_desc),M_DMAMAP,M_WAITOK))&0x1fffffff) | 0xa0000000;
-	info->drcmr_dat = 0xa2000000;	//DMA描述符地址
+	//	info->drcmr_dat = 0xa2000000;	//DMA描述符地址
+	info->drcmr_dat = (unsigned int)(malloc(sizeof(struct ls1g_nand_dma_desc)+32, M_DMAMAP, M_WAITOK));
+	info->drcmr_dat = ((info->drcmr_dat+0x1f)&(~0x1f)) & 0xfffffff | 0xa0000000;
 	if(info->drcmr_dat == NULL)
 		return -1;
 	info->drcmr_dat_phys = (info->drcmr_dat) & 0x1fffffff;	//DMA描述符物理地址
 
 	info->mmio_base = 0x1fe78000 | 0xa0000000; //NAND寄存器基地址
 
-	info->data_buff = (unsigned char *)0xa2200000;//malloc(MAX_BUFF_SIZE,M_DMAMAP,M_WAITOK);	//DMA数据缓存
-//	info->data_buff = ((unsigned int)(malloc(MAX_BUFF_SIZE,M_DMAMAP,M_WAITOK))&0x1fffffff)|0xa0000000;
+//	info->data_buff = (unsigned char *)0xa2200000;//malloc(MAX_BUFF_SIZE,M_DMAMAP,M_WAITOK);	//DMA数据缓存
+	info->data_buff = ((unsigned int)(malloc(MAX_BUFF_SIZE+32, M_DMAMAP, M_WAITOK)) + 0x1f) & (~0x1f);
+	info->data_buff = (unsigned char *)((((unsigned int)info->data_buff+0x1f)&(~0x1f)) & 0xfffffff | 0xa0000000);
 	if(info->data_buff == NULL)
 		return -1;
-	printf("data_buff==0x%08x\n",info->data_buff);
-	info->data_buff_phys = (unsigned int)(info->data_buff) & 0x1fffffff;	//DAM数据缓存物理地址
-	printf("data_buff_phys==0x%08x\n",info->data_buff_phys);
 
-//	info->data_ask = ((unsigned int)(malloc(sizeof(struct ls1g_nand_ask_regs),M_DMAMAP,M_WAITOK))&0x1fffffff)|0xa0000000;
-	info->data_ask = 0xa2400000;//malloc(sizeof(struct ls1g_nand_ask_regs),M_DMAMAP,M_WAITOK);
-	if(info->data_ask ==NULL)
+	info->data_buff_phys = (unsigned int)(info->data_buff) & 0x1fffffff;	//DAM数据缓存物理地址
+	printf("data_buff = 0x%08x\ndata_buff_phys = 0x%08x\n", info->data_buff, info->data_buff_phys);
+
+//	info->data_ask = 0xa2400000;
+	info->data_ask = (unsigned int)(malloc(sizeof(struct ls1g_nand_ask_regs)+32, M_DMAMAP, M_WAITOK));
+	info->data_ask = (((info->data_ask+0x1f)&(~0x1f)) & 0xfffffff | 0xa0000000);
+	if(info->data_ask == NULL)
 		return -1;
 	info->data_ask_phys = info->data_ask & 0x1fffffff;
 
