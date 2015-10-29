d3633fca5b8853dfde98e51927bf050b2f209595
liu shiwei <liushiwei@gmail.com>
2015-10-30 00:58:49 +0800

    编译脚本调整

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-PATH=/opt/gcc-3.4.6-2f/bin:$PATH
+PATH=/opt/gcc-3.4.6-2f/bin:`pwd`/tools/pmoncfg:$PATH
 #git pull
-make pmontools:w
-cd zloader.ls1c.openloongson
-make cfg all tgt=ram CROSS_COMPILE=mipsel-linux- LANG=C
-cp gzram ..
 
+if [ "`which bison`" ] ;then
+apt-get install bison
+fi
+
+if [ "`which flex`" ] ;then
+apt-get install flex
+fi
+
+if [ "`which makedepend`" ] ;then
+apt-get install xutils-dev
+fi
+
+make pmontools
+cd zloader.ls1c.openloongson
 make cfg all tgt=rom CROSS_COMPILE=mipsel-linux- LANG=C
 cp gzrom.bin ..
+make cfg all tgt=ram CROSS_COMPILE=mipsel-linux- LANG=C
+cp gzram ..
