9fccf29437b6801d4cda1755559bc67dfd2d3780
liu shiwei <liushiwei@gmail.com>
2016-03-03 09:47:01 +0800

    增加loongson1b开发板的编译脚本

---
+++ run_1b.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+PATH=/opt/gcc-3.4.6-2f/bin:`pwd`/tools/pmoncfg:$PATH
+#git pull
+
+if ! [ "`which bison`" ] ;then
+apt-get install bison
+fi
+
+if ![  "`which flex`" ] ;then
+apt-get install flex
+fi
+
+if ! [ "`which makedepend`" ] ;then
+apt-get install xutils-dev
+fi
+
+make pmontools
+cd zloader.ls1b.dev
+make cfg all tgt=rom CROSS_COMPILE=mipsel-linux- LANG=C
+cp gzrom.bin ..
+make cfg all tgt=ram CROSS_COMPILE=mipsel-linux- LANG=C
+cp gzram ..
