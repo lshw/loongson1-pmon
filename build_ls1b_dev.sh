42fe02d4546f9dd37da151918d636eb7d773008e
liu shiwei <liushiwei@gmail.com>
2017-10-03 01:07:50 +0800

    更换gcc4.3编译器

--- build_ls1b_dev.sh
+++ build_ls1b_dev.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-PATH=/opt/gcc-3.4.6-2f/bin:`pwd`/tools/pmoncfg:$PATH
-#git pull
+#本脚本在debian5-debian9测试没有问题,i386和amd64都可以
+if ! [ -x /opt/gcc-4.3-ls232 ]  ; then
+wget https://mirrors.ustc.edu.cn/loongson/loongson1c_bsp/gcc-4.3/gcc-4.3-ls232.tar.gz -c
+tar zxvf gcc-4.3-ls232.tar.gz -C /opt
+dpkg --add-architecture i386 #增加i386架构的libz.so.1 龙芯提供的交叉编译工具缺这个运行库
+apt-get update
+apt-get -y install zlib1g:i386
+fi
 
+PATH=/opt/gcc-4.3-ls232/bin:`pwd`/tools/pmoncfg:$PATH
+#git pull
 if ! [ "`which bison`" ] ;then
  apt-get install bison
 fi
--- build_openloongson.sh
+++ build_openloongson.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#本脚本在debian5-debian9测试没有问题
-if ! [ -x /opt/gcc-3.4.6-2f ]  ; then
-wget http://mirrors.ustc.edu.cn/loongson/loongson1b/Tools/toolchain/gcc-3.4.6-2f.tar.gz -c
-tar zxvf gcc-3.4.6-2f.tar.gz
-mkdir -p /opt
-mv gcc-3.4.6-2f /opt
+#本脚本在debian5-debian9测试没有问题,i386和amd64都可以
+if ! [ -x /opt/gcc-4.3-ls232 ]  ; then
+wget https://mirrors.ustc.edu.cn/loongson/loongson1c_bsp/gcc-4.3/gcc-4.3-ls232.tar.gz -c
+tar zxvf gcc-4.3-ls232.tar.gz -C /opt
+dpkg --add-architecture i386 #增加i386架构的libz.so.1 龙芯提供的交叉编译工具缺这个运行库
+apt-get update
+apt-get -y install zlib1g:i386
 fi
 
-PATH=/opt/gcc-3.4.6-2f/bin:`pwd`/tools/pmoncfg:$PATH
+PATH=/opt/gcc-4.3-ls232/bin:`pwd`/tools/pmoncfg:$PATH
 
 if ! [ "`which pmoncfg`" ] ; then
 cd tools/pmoncfg
