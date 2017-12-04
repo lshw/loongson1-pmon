25cc3a441ed967d2d5456aa0f09c1f1fed0bd0ff
liu shiwei <liushiwei@gmail.com>
2017-12-04 17:50:40 +0800

    修改编译脚本

--- build_openloongson.sh
+++ build_openloongson.sh
@@ -3,7 +3,8 @@
 if ! [ -x /opt/gcc-4.3-ls232 ]  ; then
 wget https://mirrors.ustc.edu.cn/loongson/loongson1c_bsp/gcc-4.3/gcc-4.3-ls232.tar.gz -c
 tar zxvf gcc-4.3-ls232.tar.gz -C /opt
-dpkg --add-architecture i386 #增加i386架构的libz.so.1 龙芯提供的交叉编译工具缺这个运行库
+#增加i386架构的libz.so.1 龙芯提供的交叉编译工具缺这个运行库
+dpkg --add-architecture i386
 apt-get update
 apt-get -y install zlib1g:i386
 fi
