23e4f765fabd50eab08beb18d53b3741affef07f
liu shiwei <liushiwei@gmail.com>
2018-09-11 16:19:54 +0800

    修改编译脚本

--- build_ls1b_dev.sh
+++ build_ls1b_dev.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #本脚本用于编译龙芯1B的pmon
 #本脚本在debian5-debian9,ubuntu16.04测试没有问题,i386和amd64都可以
+#目前不可以在win10的ubuntu子系统编译，因为它不支持i386架构
 if ! [ -x /opt/gcc-4.3-ls232 ]  ; then
 wget https://mirrors.ustc.edu.cn/loongson/loongson1c_bsp/gcc-4.3/gcc-4.3-ls232.tar.gz -c
 tar zxvf gcc-4.3-ls232.tar.gz -C /opt
 dpkg --add-architecture i386 #增加i386架构的libz.so.1 龙芯提供的交叉编译工具缺这个运行库
+fi
 apt-get update
 apt-get -y install zlib1g:i386
-fi
 
 PATH=/opt/gcc-4.3-ls232/bin:`pwd`/tools/pmoncfg:$PATH
 #git pull
