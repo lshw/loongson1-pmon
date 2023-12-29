b119ada1fcaccfe5575adc454d2b19d35f781e50
liu shiwei <liushiwei@gmail.com>
2023-12-29 19:10:44 +0800

    编译脚本支持多种rom型号

--- flashrom.sh
+++ flashrom.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #在rom文件后面补0xff ，调整大小到512k,以便flashrom 使用
+#./flashrom.sh gzrom.bin [n] [chipname]
+if ! [ "$2" ] ;then
+n=1 #//16:8M  2:1M
+else
+n=$2
+fi
+size=$(( 524288*$n ))
+echo $size
 cp $1 /tmp/$1.tmp
-printf "%0.s\xff" {1..524288} >>/tmp/$1.tmp
-dd if=/tmp/$1.tmp of=/tmp/$1.512k count=1 bs=524288
+dd if=/dev/zero count=524288 bs=$n |sed 's/\x00/\xff/g' >>/tmp/$1.tmp 2>/dev/null
+ls -l /tmp/$1.tmp
+dd if=/tmp/$1.tmp of=/tmp/$1.resize count=524288 bs=$n >/dev/null 2>/dev/null
 rm /tmp/$1.tmp
-flashrom -p ch341a_spi -w /tmp/$1.512k
-rm /tmp/$1.512k
+if [ "$3" ] ; then
+chipname="-c $3"
+fi
+flashrom -p ch341a_spi $chipname -w /tmp/$1.resize
