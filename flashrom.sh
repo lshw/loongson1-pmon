1877a6e7c5b706297cc63bcc4879ce8008a6af53
liu shiwei <liushiwei@gmail.com>
2024-05-02 14:26:41 +0000

    flashrom.sh 先读取原来的rom ， 然后合并nvram到新rom, 再写入

--- flashrom.sh
+++ flashrom.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-#在rom文件后面补0xff ，调整大小到512k,以便flashrom 使用
-#./flashrom.sh gzrom.bin [n] [chipname]
-if ! [ "$2" ] ;then
-n=1 #//16:8M  2:1M
-else
-n=$2
+#./flashrom.sh gzrom.bin [chipname]
+
+if ! [ "$1" ] ; then
+  echo $0 gzrom.bin [W25X40]
+  exit
 fi
-size=$(( 524288*$n ))
-echo $size
-cp $1 /tmp/$1.tmp
-dd if=/dev/zero count=524288 bs=$n |sed 's/\x00/\xff/g' >>/tmp/$1.tmp 2>/dev/null
-ls -l /tmp/$1.tmp
-dd if=/tmp/$1.tmp of=/tmp/$1.resize count=524288 bs=$n >/dev/null 2>/dev/null
-rm /tmp/$1.tmp
-if [ "$3" ] ; then
-chipname="-c $3"
+
+if ! [ -e $1 ] ; then
+  echo not find $1
+  exit
 fi
+size=$( ls -l $1|awk '{print $5}' )
+if [ "$2" ] ; then
+ chipname="-c $2"
+fi
+rm -f /tmp/$1.tmp
+flashrom -p ch341a_spi $chipname -r /tmp/$1.tmp
+cp -f $1 /tmp/$1.resize
+dd if=/tmp/$1.tmp of=/tmp/$1.resize skip=$size seek=$size bs=1
+ /tmp/$1.tmp
 flashrom -p ch341a_spi $chipname -w /tmp/$1.resize
